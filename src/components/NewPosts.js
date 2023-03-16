731d0913f00ca30a8ad27f30bdfb29059e85e6ef
Afmjuniors <afmjuniors@gmail.com>
2023-03-15 21:18:05 -0300

    feat:spinner btn new post

--- src/components/NewPosts.js
+++ src/components/NewPosts.js
@@ -1,3 +1,4 @@
+import { CircularProgress } from '@mui/material'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
@@ -5,29 +6,31 @@ import { BASE_URL } from '../constants/baseUrl'
 
 const NewPosts = () => {
   const [isLoading, setIsLoading] = useState(false)
+  const [isLoadingBtn, setIsLoadingBtn] = useState(false)
   const [content, setContent] = useState('')
-  const [headers, setHeaders]= useState({})
+  const [headers, setHeaders] = useState({})
   const [path, setPath] = useState('/posts')
   const params = useParams()
-  useEffect(()=>{
+  useEffect(() => {
     setHeaders({
-      authorization:localStorage.getItem('token')
+      authorization: localStorage.getItem('token')
     })
-    if(params.id){
+    if (params.id) {
       setPath(`/comments/${params.id}`)
-    }else{
+    } else {
       setPath('/posts')
     }
 
 
-  },[])
+  }, [])
 
-  const createNewPost = async (headers) =>{
+  const createNewPost = async (headers) => {
     try {
       setIsLoading(true)
-      const response = await axios.post(`${BASE_URL}${path}`,{content},{headers})
+      const response = await axios.post(`${BASE_URL}${path}`, { content }, { headers })
       console.log(response)
       setIsLoading(false)
+      setIsLoadingBtn(false)
       window.location.reload()
     } catch (error) {
       setIsLoading(false)
@@ -35,8 +38,8 @@ const NewPosts = () => {
 
     }
   }
-  const handleClickPost = () =>{
-    console.log(content)
+  const handleClickPost = () => {
+    setIsLoadingBtn(true)
     createNewPost(headers)
     setContent('')
   }
@@ -44,15 +47,19 @@ const NewPosts = () => {
 
   return (
     <div className='new-post'>
-        <textarea className='textarea-newpost'
-        placeholder={path.includes("posts")?"Escreva seu post...":"Adicionar comentário"}
-         value={content} onChange={(e)=>setContent(e.target.value)} rows="6">
+      <textarea className='textarea-newpost'
+        placeholder={path.includes("posts") ? "Escreva seu post..." : "Adicionar comentário"}
+        value={content} onChange={(e) => setContent(e.target.value)} rows="6">
 
-        </textarea>
-        <button onClick={handleClickPost} className='btn btn-new-post'> 
-        {path.includes("posts")?<span>Postar</span>:<span>Responder</span>}
-        </button>
-       <div className='line'></div>
+      </textarea>
+      <button onClick={handleClickPost} className='btn btn-new-post'> {
+        isLoadingBtn ?
+          <CircularProgress color="inherit" /> : <>
+            {path.includes("posts") ? <span>Postar</span> : <span>Responder</span>}
+          </>
+      }
+      </button>
+      <div className='line'></div>
     </div>
   )
 }

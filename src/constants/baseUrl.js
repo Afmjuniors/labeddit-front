dea563b657de59f5a52c0eec380fa757af231364
Alexandre Machado <111317688+Afmjuniors@users.noreply.github.com>
2023-03-17 13:06:02 -0300

    Update baseUrl.js

--- src/constants/baseUrl.js
+++ src/constants/baseUrl.js
@@ -1,2 +1,2 @@
- export const BASE_URL = "http://ec2-100-26-232-191.compute-1.amazonaws.com:3003"
-//export const BASE_URL = "https://labeddit-api.onrender.com"
+// export const BASE_URL = "http://ec2-100-26-232-191.compute-1.amazonaws.com:3003"
+export const BASE_URL = "https://labeddit-api.onrender.com"
